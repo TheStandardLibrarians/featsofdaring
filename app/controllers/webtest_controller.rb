7e4721b80fe4bce344756df6f2ce309fdb401c4e
Jalil Mohammed, Jen Diamond, Omowale Oniyide, Robert Wilkinson & Stephanie Betancourt <thestandardlibrarians+jalil+jen+omowale+robert+stephanie@gmail.com>
2014-07-24 01:50:52 +0000

    adds sample plugin to webtest controller

--- app/controllers/webtest_controller.rb
+++ app/controllers/webtest_controller.rb
@@ -1,5 +1,13 @@
 class WebtestController < ApplicationController
   def index
+    Ripl::Shell.include Ripl::RedError
     render text: Ripl.shell.eval_input("RUBY_VERSION")
+  end
+  module Ripl
+    module RedError
+      def format_error(error)
+        "\e[31m#{super}\e[m"
+      end
+    end
   end
 end
