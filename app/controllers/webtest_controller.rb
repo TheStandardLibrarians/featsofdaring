1248caa449938e6c71c2ce5dd4357f29cf175cdc
Jen Diamond, Josh Loper, Omowale Oniyide, Robert Wilkinson & Stephanie Betancourt <thestandardlibrarians+jen+josh+omowale+robert+stephanie@gmail.com>
2014-07-24 03:53:17 +0000

    Redirects the user with "meow" and renders RIPL eval
    [#75007238]

--- app/controllers/webtest_controller.rb
+++ app/controllers/webtest_controller.rb
@@ -1,13 +1,10 @@
 class WebtestController < ApplicationController
+
   def index
-    Ripl::Shell.include Ripl::RedError
-    render text: Ripl.shell.eval_input("RUBY_VERSION")
-  end
-  module Ripl
-    module RedError
-      def format_error(error)
-        "\e[31m#{super}\e[m"
-      end
+    if params[:user_input] == "meow"
+       redirect_to "http://www.pinterest.com" 
+    else
+      render text: ::Ripl.shell.eval_input(params[:user_input])
     end
   end
 end
