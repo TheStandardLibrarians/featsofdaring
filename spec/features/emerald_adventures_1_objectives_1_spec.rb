6372fb1f0fc15357436004b14a7e376572870211
Jalil Mohammed, Jen Diamond, Omowale Oniyide, Robert Wilkinson & Stephanie Betancourt <thestandardlibrarians+jalil+jen+omowale+robert+stephanie@gmail.com>
2014-07-30 01:56:42 +0000

    Emerald learns about making calls on the web
    Finishes putting the narrative in the browser. Uses I18n#translate to abstract the copy into the english translations.
    
    [finishes #74573768]

--- spec/features/emerald_adventures_1_objectives_1_spec.rb
+++ spec/features/emerald_adventures_1_objectives_1_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature "Emerald visits adventure 1 objective 1" do
   scenario "emerald reads narrative" do
     visit '/adventures/1/objectives/1'
-    expect(page).to have_content 'PrettyP has moved to Hollywood.'  
+    expect(page).to have_content I18n.t("adventure.one.objective1.copy")
   end
 end
