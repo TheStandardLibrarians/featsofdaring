903300c9354998f6856d7b114a0db844aa40b6b1
Jen Diamond, Josh Loper & Robert Wilkinson <thestandardlibrarians+jen+josh+robert@gmail.com>
2014-07-31 00:05:22 +0000

    Adds spec/controller test to pass em gets a 301 error
    Adds spec/controller test to pass Emerald gets a 301 Error and removes feature test.
    [#75865326]
    
    	renamed:    spec/controllers/status_controller.rb -> spec/controllers/status_controller_spec.rb
    	deleted:    spec/features/emerald_gets_a_301_spec.rb

--- spec/features/emerald_gets_a_301_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-feature "Emerald calls URLs which return status codes" do
-  scenario "emerald gets a 301" do
-    visit "http://featsofdaring.herokuapp.com/prettyp"
-    expect(page.status_code).to eq 301
-  end
-end
