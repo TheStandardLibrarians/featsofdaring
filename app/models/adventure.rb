069e086ba95ffe2adefcb6589f64cda23aa1c367
Jen Diamond, Mike McCormick, Omowale Oniyide & Robert Wilkinson <thestandardlibrarians+jen+mike+omowale+robert@gmail.com>
2014-08-06 03:43:54 +0000

    Sets up domain object models
    Adds objective, adventure and narrative classes
    [#76334086]
    
     Changes to be committed:
    	new file:   app/assets/stylesheets/application.css.scss
    	new file:   app/assets/stylesheets/bootstrap_and_customization.css.scss
    	new file:   app/models/adventure.rb
    	new file:   app/models/narrative.rb
    	new file:   app/models/objective.rb
    	renamed:    spec/models/adventures_spec.rb -> spec/models/adventure_spec.rb
    	new file:   spec/models/narrative_spec.rb
    	new file:   spec/models/objective_spec.rb

---
+++ app/models/adventure.rb
@@ -0,0 +1,8 @@
+class Adventure
+  attr_accessor :title, :introduction, :id 
+  def initialize (title, introduction, id) 
+    @title = title
+    @introduction = introduction
+    @id = id
+  end
+end
---
+++ spec/models/narrative_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+describe Narrative do
+  describe "Can we construct an instance of class Narrative" do
+    it "attempts to create new Narrative class" do
+      @narrative = Narrative.new("1", "1", "learn", "help cuddy", "www.example.com/picture.jpg") 
+      expect(@narrative.id).to eq("1")
+      expect(@narrative.objective_id).to eq("1")
+      expect(@narrative.type).to eq("learn")
+      expect(@narrative.text).to eq("help cuddy")
+      expect(@narrative.image_url).to eq("www.example.com/picture.jpg")
+    end
+  end
+end
