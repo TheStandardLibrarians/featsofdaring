056563d3135f3827933254ed6a81077218147dd7
Jen Diamond, Josh Loper, Omowale Oniyide, Pat Maddox, Robert Wilkinson & Stephanie Betancourt <thestandardlibrarians+jen+josh+omowale+pat+robert+stephanie@gmail.com>
2014-08-07 01:30:03 +0000

    Adds meow and back commands to move back and forth through objectives
    Adds before_action filter commands to allow 'meow' and 'back' to move
    between pages
    
    [#74574144]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,26 @@ module ApplicationHelper
   end
  private
 
- def commands
-    if params[:ripl_input] == "meow"
+  def commands
+    meow_command
+    back_command
+  end
+
+  def meow_command
+    if params[:ripl_input] =~ /[mM][eE][oO][wW]+/
       params[:id] = params[:id].to_i + 1
-      redirect_to action:  'show', adventure_id: params[:adventure_id], id: params[:id]
+      redirect_to_objective
     end
+  end
+ 
+  def back_command
+    if params[:ripl_input] =~ /[bB][aA][cC][kK]+/ && params[:id] != 1
+      params[:id] = params[:id].to_i - 1
+      redirect_to_objective
+    end
+  end
+
+  def redirect_to_objective
+   redirect_to action: 'show', adventure_id: params[:adventure_id], id: params[:id]
   end
 end
