929a7bfd0b4f60bb9a71f17eae409376f66e2ee7
Robert Wilkinson <thestandardlibrarians+robert@gmail.com>
2014-08-08 21:55:36 +0000

    Creates new tests to check that link to adv1 is on front page with repl and introduction copy
    Renders two partials on the front page , left side and right side that have the introduction and repl in them respectively. Changes the ripl to text_area. Refactors the intro copy into introduction.yml.
    
    [#76497278]

--- spec/features/emerald_comes_to_the_site_spec.rb
+++ spec/features/emerald_comes_to_the_site_spec.rb
@@ -5,4 +5,17 @@ feature 'Emerald comes to the site' do
    visit '/'
    expect(page).to have_content 'Adventure 1'
  end
+ scenario "emerald views intro text" do
+   visit "/"
+   expect(page).to have_content I18n.t("introduction.copy")
+ end
+ scenario "Emerald sees link_to Adventure one" do
+   visit '/'
+   click_link 'Adventure 1'
+   expect(page).to have_content "Oh no!"
+ end
+ scenario "Emerald Sees repl on Home Page" do
+   visit '/'
+   expect(page).to have_css 'textarea#ripl_input'
+   end
 end
