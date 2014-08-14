11c1ed867ec14f4ce983cea2ce60f9b8194cb85b
Jen Diamond, John Ryan, Josh Loper, Omowale Oniyide, Robert Wilkinson & Stephanie Betancourt <thestandardlibrarians+jen+john+josh+omowale+robert+stephanie@gmail.com>
2014-08-14 04:22:02 +0000

    Adds test to check for find method on Narrative, makes narrative have unique pair
    Refactors to give narrative a unique id 'pair_id', allows lookup by adventure_id, objective_id, and type
    
    [#76497278]
     On branch styling
     Changes to be committed:
    	modified:   app/models/narrative.rb
    	new file:   mob_order.rb
    	modified:   spec/models/narrative_spec.rb

--- app/models/narrative.rb
+++ app/models/narrative.rb
@@ -1,19 +1,25 @@
 class Narrative
-  attr_accessor :id, :objective_id, :type, :copy, :image_url
+  attr_accessor :id, :pair_id, :type, :copy, :image_url
 
-  def initialize(id, objective_id, type, copy, image_url)
+  def initialize(id, adventure_id, objective_id, type, copy, image_url)
     @id = id
-    @objective_id = objective_id
+    @pair_id = { 
+		adventure_id: adventure_id, 
+		objective_id:  objective_id
+		}
     @type = type
     @copy = copy
     @image_url = image_url
   end
-  NARRATIVES = [
-    Narrative.new(1, 1, 'learn', 'copy', 'image_url'),
-    Narrative.new(2, 1, 'submit', 'copy', 'image_url')
-  ]
-
-  def self.find_by_objective_id(objective_id)
-    NARRATIVES.find_all { |value| value.objective_id.to_i == objective_id }
+NARRATIVES = [
+  Narrative.new(1, 1, 1, 'learn', 'copy', 'image_url'),
+  Narrative.new(2, 2, 1, 'submit', 'copy', 'image_url')
+]
+  def self.find_by_pair(adventure_id, objective_id, type)
+    NARRATIVES.find do |narrative|  
+      narrative.pair_id[:adventure_id] == adventure_id &&
+      narrative.pair_id[:objective_id] == objective_id &&
+      narrative.type == type 
+    end
   end
 end
