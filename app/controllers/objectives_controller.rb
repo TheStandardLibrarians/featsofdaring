aa04b597c6a04e88a10909f5aceac9b82a84a217
Jen Diamond, Omowale Oniyide & Ryan Moran <thestandardlibrarians+jen+omowale+ryan@gmail.com>
2014-08-26 04:18:19 +0000

    Displays adventure title on objective page.
    [#76497278]

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -2,10 +2,8 @@ class ObjectivesController < ApplicationController
   include ApplicationHelper
   before_action :commands
   def show
-    #find_adventure_and_objective  
-    puts "@objective1 works" 
-    puts @objective = Objective.find(params[:adventure_id], params[:id])
-    #@adventure = Adventure.find(params[:adventure_id])
+    @objective = Objective.find(params[:adventure_id], params[:id])
+    @adventure = Adventure.find(params[:adventure_id])
   end
   def update
     render plain: evaluate(params[:ripl_input])
