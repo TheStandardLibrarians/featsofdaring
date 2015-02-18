7cf8a08052a1c3f279e5e16c36c9907194a22301
Jen Diamond <thejendiamond@gmail.com>
2015-02-17 21:44:37 -0800

    Fix url

--- spec/features/emerald_adventure_1_objective_2_spec.rb
+++ spec/features/emerald_adventure_1_objective_2_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-feature 'Emerald goes to adv1/obj2 page' do
-  scenario 'Emerald enters adventure/1/objective/learn as url' do
+feature 'Emerald goes to adventures/1/objectives/2/learn page' do
+  scenario 'Emerald enters adventures/1/objectives/2/learn as url' do
     visit 'adventures/1/objectives/2/learn'
     expect(page).to have_content 'Objective 2'
   end
